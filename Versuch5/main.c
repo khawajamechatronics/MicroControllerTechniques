fba575f41cbbb32df05fb57e858be0bfbfd2fb7d
Tobias Faller <TobiasFaller@gmx.net>
2017-04-30 19:39:53 +0200

    Implemented playing of both melodies

--- Versuch5/main.c
+++ Versuch5/main.c
@@ -15,7 +15,7 @@
 #undef MELODY_NAME
 
 // Comment out to activate static melody
-#define STATIC_MELODY
+// #define STATIC_MELODY
 
 #define BUTTON_PLAY BIT3
 #define BUTTON_PAUSE BIT4
@@ -23,18 +23,36 @@
 
 typedef enum state {
   PAUSED,
-  RUNNING
+  RUNNING,
+#ifndef STATIC_MELODY
+  WAITING
+#endif
 } state_t;
 
 __inline void setup(void);
 __inline void loop(void);
 
+#ifdef STATIC_MELODY
 void melody_play(uint8_t melody);
 void melody2_play(void);
 void melody_finished(void);
+#else
+__inline void pause_resume(void);
 
+void melody_finished(void);
+void button_measure(void);
+void button_reactivate(void);
+#endif
+
+#ifdef STATIC_MELODY
 static uint8_t current_melody;
+#endif
+
 static state_t current_state;
+
+#ifndef STATIC_MELODY
+static uint8_t button_press_count;
+#endif
 
 /*
  * MSP430G2553
@@ -61,7 +79,8 @@ __inline void setup() {
   // Initialize player
   play_init();
 
-  current_state = RUNNING;
+  // Initialize in non-paused state
+  current_state = WAITING;
 
 #ifdef STATIC_MELODY
   // Initialize pause button
@@ -73,6 +92,9 @@ __inline void setup() {
   P1IES |= BUTTON_PAUSE; // Interrupt on high-to-low transition
   P1IFG &= ~BUTTON_PAUSE; // Reset interrupt flag
   P1IE |= BUTTON_PAUSE; // Enable interrupt
+
+  // Play first melody
+  melody_play(1);
 #else
   // Initialize buttons
   P1SEL &= ~BUTTONS; // Set as IO port
@@ -82,11 +104,11 @@ __inline void setup() {
   P1OUT |= BUTTONS; // Pull-up
   P1IES |= BUTTONS; // Interrupt on high-to-low transition
   P1IFG &= ~BUTTONS; // Reset interrupt flag
-  P1IE |= BUTTONS; // Enable interrupt
-#endif
+  P1IE |= BUTTON_PLAY; // Enable interrupt
 
-  // Play first melody
-  melody_play(1);
+  // No button was pressed
+  button_press_count = 0;
+#endif
 }
 
 __inline void loop() { }
@@ -170,20 +192,97 @@ __interrupt void button_pressed(void) {
 #ifndef STATIC_MELODY
 #pragma vector=PORT1_VECTOR
 __interrupt void button_pressed(void) {
-  uint8_t flags = P1IFG; // Save interrupt flags
+  uint8_t flags = P1IFG & BUTTONS; // Save interrupt flags
+  uint8_t enable = P1IE & BUTTONS; // Save enabled flags
 
-  P1IE &= ~BUTTONS; // Disable interrupt
-  P1IFG &= ~BUTTONS; // Reset interrupt flag
+  P1IE &= ~BUTTONS; // Disable interrupts
 
   // Handle button press
   if (flags & BUTTON_PAUSE) {
-    pause();
+    pause_resume();
 
-    // Re-enable interrupt
-    P1IE |= BUTTONS;
+    // Reset interrupt flag for pause button
+    P1IFG &= ~BUTTON_PAUSE;
+    P1IE |= enable | BUTTON_PAUSE; // Re-enable interrupt
     return;
-  } else if (flags & BUTTON_PLAY) {
-    // TODO
   }
+
+  if (flags & BUTTON_PLAY) {
+    // No button was pressed
+    if (button_press_count == 0) {
+      // Schedule measurement
+      wait(16, &button_measure);
+    }
+
+    // Increment button press count
+    button_press_count++;
+
+    // Reset interrupt flag for play button
+    P1IFG &= ~BUTTON_PLAY;
+    P1IE |= enable | BUTTON_PLAY; // Re-enable interrupt
+    return;
+  }
+
+  // Hang since this is an unwanted case (for debugging)
+  for (;;);
+}
+
+__inline void pause_resume(void) {
+  static uint8_t t1_state;
+  static uint8_t t2_state;
+
+  switch (current_state) {
+  case WAITING:
+    return; // No melody is playing
+
+  case PAUSED:
+    // Set state to playing
+    current_state = RUNNING;
+
+    // Resume state
+    TA0CTL |= t1_state;
+    TA1CTL |= t2_state;
+    break;
+
+  case RUNNING:
+    // Save the state of both timer
+    t1_state = TA0CTL & MC_1;
+    t2_state = TA1CTL & MC_1;
+
+    TA0CTL &= ~MC_1; // Deactivate timer
+    TA1CTL &= ~MC_1; // Deactivate timer
+
+    current_state = PAUSED;
+    break;
+  }
+
+  // Wait for button to get released
+  while (!(P1IN & BUTTON_PAUSE));
+}
+
+void button_measure(void) {
+  // Disable play button
+  P1IE &= ~BUTTON_PLAY;
+
+  // Enable pause button
+  P1IE |= BUTTON_PAUSE;
+
+  current_state = RUNNING;
+  if (button_press_count == 1) {
+    play_melody(&MELODY1, &button_reactivate);
+  } else {
+    play_melody(&MELODY2, &button_reactivate);
+  }
+}
+
+void button_reactivate(void) {
+  // Reset button press count
+  button_press_count = 0;
+
+  // Disable pause button
+  P1IE &= ~BUTTON_PAUSE;
+
+  // Enable play button
+  P1IE |= BUTTON_PLAY;
 }
 #endif

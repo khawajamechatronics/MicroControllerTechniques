8323368c98cd695cbe333c56166fe618880bec1f
Tobias Faller <TobiasFaller@gmx.net>
2017-05-25 02:33:28 +0200

    Updated tetris row-clear implementation

--- Faller_Projekt/src/tetris.c
+++ Faller_Projekt/src/tetris.c
@@ -12,6 +12,8 @@
 
 #include "tetris_p.h"
 
+// --- Game -------------------------------------------------------------------
+
 __inline void
 tetris_init_game (tetris_t *tetris)
 {
@@ -35,13 +37,71 @@ tetris_start_game (tetris_t *tetris)
 static __inline void
 tetris_step_game(tetris_t *tetris)
 {
+  field_t *current_field = tetris_get_current_field(tetris);
+  field_t *old_field = tetris_get_last_field(tetris);
 
+  tetris_clear_full_lines(current_field, old_field);
 }
+
+static tetromino_t
+tetris_pick_random_tetromino (void)
+{
+  // Use the default random number generator
+  return (tetromino_t) (rand() % 7);
+}
+static __inline void
+tetris_clear_full_lines (field_t *field, field_t *old_field)
+{
+  findex_t y;
+  findex_t x;
+  findex_t dy;
+
+  // Update from bottom up
+  for (y = GAME_HEIGHT - 1; y < GAME_HEIGHT; --y) {
+    // Check each item in row
+    for (x = 0; x < GAME_WIDTH; ++x) {
+      field_item_t *item = tetris_field_item_get_at(field, x, y);
+
+      if (tetris_field_item_is_empty(item))
+        goto next_row;
+    }
+
+    // Row is full -> drop rows from bottom to top
+    for (dy = y; dy < GAME_HEIGHT; --dy) {
+      for (x = 0; x < GAME_WIDTH; ++x) {
+        findex_t index = tetris_field_item_get_index(x, dy);
+
+        if (dy == 0) { // Clear top row
+          tetris_field_item_set(field, old_field, index, TETRIS_FIELD_EMPTY);
+        } else {
+          findex_t upper_index = tetris_field_item_get_index(x, dy - 1);
+          field_item_t *upper_item
+            = tetris_field_item_get(old_field, upper_index);
+
+          tetris_field_item_set(field, old_field, index,
+                                tetris_field_item_get_tetromino(upper_item));
+        }
+      }
+    }
+
+next_row:
+    ;
+  }
+}
+
+
+// --- Field ------------------------------------------------------------------
 
 static __inline field_t*
 tetris_get_current_field (tetris_t *tetris)
 {
   return &tetris->buffer[tetris->current_buffer];
+}
+
+static __inline field_t*
+tetris_get_last_field (tetris_t *tetris)
+{
+  return &tetris->buffer[tetris->last_buffer];
 }
 
 static __inline void
@@ -52,26 +112,73 @@ tetris_switch_field (tetris_t *tetris) {
     tetris->current_buffer = 0;
 }
 
-static __inline field_item_t*
-tetris_field_item_get (field_t *field, uint8_t x, uint8_t y)
+// --- Item -------------------------------------------------------------------
+
+static __inline findex_t
+tetris_field_item_get_index (findex_t x, findex_t y)
 {
-  return &field->data[y * GAME_WIDTH + x];
+  return y * GAME_WIDTH + x;
+}
+
+static __inline field_item_t*
+tetris_field_item_get_at (field_t *field, findex_t x, findex_t y)
+{
+  findex_t index = tetris_field_item_get_index(x, y);
+  return tetris_field_item_get(field, index);
+}
+
+static __inline field_item_t*
+tetris_field_item_get (field_t *field, findex_t index)
+{
+  return &field->data[index];
+}
+
+static __inline tetromino_t
+tetris_field_item_get_tetromino (field_item_t *item)
+{
+  return (*item & ~(TETRIS_FIELD_UPDATED | TETRIS_FIELD_TEMP));
+}
+
+static __inline tetromino_t
+tetris_field_item_get_value (field_item_t *item)
+{
+  return (*item & ~TETRIS_FIELD_UPDATED);
+}
+
+static __inline void
+tetris_field_item_set (field_t *field, field_t *old_field, findex_t index,
+                       field_item_t value)
+{
+  bool_t changed = (field == old_field);
+
+  if (!changed) {
+    field_item_t *old_item = tetris_field_item_get(old_field, index);
+    changed = (tetris_field_item_get_value(old_item) != value);
+  }
+
+  field_item_t *item = tetris_field_item_get(field, index);
+  *item = value;
+
+  if (!changed)
+    return;
+
+  *item |= TETRIS_FIELD_UPDATED;
 }
 
 static __inline bool_t
 tetris_field_item_is_empty (const field_item_t *item)
 {
-  return ((*item & ~(TETRIS_FIELD_UPDATED | TETRIS_FIELD_TEMP)) == 0);
+  return (tetris_field_item_get_tetromino(item) == TETRIS_FIELD_EMPTY);
 }
 
 static __inline void
-tetris_field_set_updated(field_item_t *item) {
+tetris_field_item_set_updated (field_item_t *item)
+{
   *item |= TETRIS_FIELD_UPDATED;
 }
 
-static tetromino_t
-tetris_pick_random_tetromino (void)
+static __inline void
+tetris_field_item_set_temp (field_item_t *item)
 {
-  // Use the default random number generator
-  return (tetromino_t) (rand() % 7);
+  *item |= TETRIS_FIELD_EMPTY;
 }
--- Faller_Projekt/src/tetris_p.h
+++ Faller_Projekt/src/tetris_p.h
@@ -14,12 +14,21 @@
 // Definitions
 // ----------------------------------------------------------------------------
 
+#define TETRIS_FIELD_EMPTY 0x00
 #define TETRIS_FIELD_UPDATED 0x80
 #define TETRIS_FIELD_TEMP 0x40
 
 // ----------------------------------------------------------------------------
+// Types
+// ----------------------------------------------------------------------------
+
+typedef uint16_t findex_t;
+
+// ----------------------------------------------------------------------------
 // Methods
 // ----------------------------------------------------------------------------
+
+// --- Game -------------------------------------------------------------------
 
 /**
  * Returns a random tetromino.
@@ -30,12 +39,22 @@ static tetromino_t
 tetris_pick_random_tetromino (void);
 
 /**
- * Sets an location as updated which is then send to the console.
+ * Updates the game field and drops the tetromino.
  *
- * @param item The location which was changed
+ * @param tetris The main tetris instance
  */
 static __inline void
-tetris_field_set_updated (field_item_t *item);
+tetris_step_field (tetris_t *tetris);
+
+/**
+ * Clears all full lines in the field and drops all lines above.
+ *
+ * @param field The field to clear full lines
+ */
+static __inline void
+tetris_clear_full_lines (field_t *field, field_t *old_field);
+
+// --- Field ------------------------------------------------------------------
 
 /**
  * Returns the current game field to work on.
@@ -47,6 +66,15 @@ static __inline field_t*
 tetris_get_current_field (tetris_t *tetris);
 
 /**
+ * Returns the last game field which is currently transmitted.
+ *
+ * @param tetris The main tetris instance
+ * @return A pointer to the last game field
+ */
+static __inline field_t*
+tetris_get_last_field (tetris_t *tetris);
+
+/**
  * Switches the current game field to enable updating the display while
  * drawing the next frame.
  *
@@ -55,24 +83,38 @@ tetris_get_current_field (tetris_t *tetris);
 static __inline void
 tetris_switch_field (tetris_t *tetris);
 
-/**
- * Updates the game field and drops the tetromino.
- *
- * @param tetris The main tetris instance
- */
-static __inline void
-tetris_step_field (tetris_t *tetris);
+// --- Item -------------------------------------------------------------------
 
 /**
  * Returns a pointer to the location on the tetris field.
  *
  * @param field The current tetris field
- * @param x The x coordinate to get
- * @param y The y coordinate to get
+ * @param index The index of the item
  * @return The item at the specified position
  */
 static __inline field_item_t*
-tetris_field_item_get (field_t *field, uint8_t x, uint8_t y);
+tetris_field_item_get (field_t *field, findex_t index);
+
+/**
+ * Returns a pointer to the location on the tetris field.
+ *
+ * @param field The current tetris field
+ * @param x The x coordinate
+ * @param y The y coordinate
+ * @return The item at the specified position
+ */
+static __inline field_item_t*
+tetris_field_item_get_at (field_t *field, findex_t x, findex_t y);
+
+/**
+ * Returns the index of the specified location.
+ *
+ * @param x The x coordinate
+ * @param y The y coordinate
+ * @return The index to access the item
+ */
+static __inline findex_t
+tetris_field_item_get_index (findex_t x, findex_t y);
 
 /**
  * Returns if the location on the tetris field is empty.
@@ -82,5 +124,21 @@ tetris_field_item_get (field_t *field, uint8_t x, uint8_t y);
  */
 static __inline bool_t
 tetris_field_item_is_empty (const field_item_t *item);
+
+/**
+ * Sets the updated flag of the item.
+ *
+ * @param item The item to update
+ */
+static __inline void
+tetris_field_item_set_updated (field_item_t *item);
+
+/**
+ * Sets the temporary flag of the item.
+ *
+ * @param item The item to update
+ */
+static __inline void
+tetris_field_item_set_temp (field_item_t *item);
 
 #endif // !__TETRIS_P_H
